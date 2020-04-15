be519da53768174676cb984c334d9dff991c6e29
matheus mota andrelino <matheusmotaandrelino@gmail.com>
2020-04-15 02:36:28 -0300

    metos de lista

--- index.php
+++ index.php
@@ -2,11 +2,27 @@
 
 require_once("config.php");
 
+//Carrega um usuário
+/*
 $user = new Usuario();
-
 $user->loadbyId(1);
-
 echo $user;
+*/
 
+//Carrega uma lista de usuários
+/*
+$lista = Usuario::getList();
+echo json_encode($lista);
+*/
+
+//Carrega uma lista de usuarios buscando pelo login
+/*$search = Usuario::search("us");
+echo json_encode($search);*/
+
+//Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("user", "12345");
+
+echo $usuario;
 
 ?>
